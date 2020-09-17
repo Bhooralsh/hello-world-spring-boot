95571ab7221250c7d5de9d838e68150bd11529b0
Thinkpad <Thinkpad@DESKTOP-GOJQCJO.mshome.net>
2020-09-17 14:11:54 -0700

    first commit

---
+++ HelloWorld/src/main/java/com/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	
+	@RequestMapping("hello")
+	public String hello() {
+		
+		return "index.jsp";
+	}
+
+}
